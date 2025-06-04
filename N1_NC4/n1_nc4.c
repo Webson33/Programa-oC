9685e4ca790f0403c7454a35f88f59535a1f7cbf
Webson33 <websonsan@gmail.com>
2025-06-04 09:23:29 +0000

    Meus arquivos.

---
+++ N1_NC4/n1_nc4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 23;
+    int quantidade = 1;
+    float altura = 1.74;
+    double peso = 82.0;
+    char letra = 'A';
+    char nome[20] = "Webson";
+
+    idade = 23;
+
+    quantidade = 10;
+    
+}
